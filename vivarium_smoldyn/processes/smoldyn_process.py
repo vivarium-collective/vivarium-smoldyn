97a8e3c17ee74bb7328106bde5a3ac8c15faf7e2
Eran Agmon <agmon.eran@gmail.com>
2021-05-26 14:53:28 -0700

    pass parameters, get data

--- vivarium_smoldyn/processes/smoldyn_process.py
+++ vivarium_smoldyn/processes/smoldyn_process.py
@@ -19,7 +19,12 @@ class SmoldynProcess(Process):
     defaults = {
         'animate': False,
         'time_step': 10,
+        'low': [0, 0],
+        'high': [10, 10],
+        'boundary': 'p',
         'dt': 0.1,
+        'species': {},
+        'reactions': {},
     }
 
     def __init__(self, parameters=None):
@@ -29,31 +34,39 @@ class SmoldynProcess(Process):
 
         # initialize the simulation
         self.smoldyn = sm.Simulation(
-            low=[0, 0],
-            high=[10, 10],
-            types="p",
-            # quit_at_end=True,
+            low=self.parameters['low'],
+            high=self.parameters['high'],
+            types=self.parameters['boundary'],
         )
+        self.smoldyn.addOutputData('counts')
+        self.smoldyn.addCommand(cmd='molcount counts', cmd_type='E')
 
-        # species X A B A2 B2
-        X = self.smoldyn.addSpecies("X", difc=0, color="green", display_size=3)
-        A = self.smoldyn.addSpecies("A", difc=1, color="red", display_size=3)
-        B = self.smoldyn.addSpecies("B", difc=1, color="blue", display_size=3)
-        A2 = self.smoldyn.addSpecies("A2", difc=1, color="red", display_size=5)
-        B2 = self.smoldyn.addSpecies("B2", difc=1, color="blue", display_size=5)
+        # make the species
+        self.species = {}
+        for name, config in self.parameters['species'].items():
+            solution = None
+            if 'solution' in config:
+                solution = config.pop('solution')
+            self.species[name] = self.smoldyn.addSpecies(name, **config)
+            if solution:
+                self.species[name].addToSolution(
+                    solution.get('counts'),
+                    pos=solution.get('position')
+            )
 
-        # mol 1 X 5 5
-        X.addToSolution(1, pos=[5, 5])
+        self.reactions = {}
+        for rxn_name, config in self.parameters['reactions'].items():
+            substrate_names = config.pop('subs')
+            product_names = config.pop('prds')
+            substrates = [self.species[name] for name in substrate_names]
+            products = [self.species[name] for name in product_names]
+            self.reactions[rxn_name] = self.smoldyn.addReaction(
+                rxn_name,
+                subs=substrates,
+                prds=products,
+                **config
+            )
 
-        express = self.smoldyn.addReaction("express", subs=[X], prds=[X, A, B], rate=1)
-        Adimer = self.smoldyn.addBidirectionalReaction("Adimer", subs=[A, A], prds=[A2], kf=1, kb=1)
-        Bdimer = self.smoldyn.addBidirectionalReaction("Bdimer", subs=[B, B], prds=[B2], kf=1, kb=1)
-        AxB = self.smoldyn.addReaction("AxB", subs=[A2, B], prds=[A2], rate=1)
-        BxA = self.smoldyn.addReaction("BxA", subs=[B2, A], prds=[B2], rate=1)
-        Adegrade = self.smoldyn.addReaction("Adegrade", subs=[A], prds=[], rate=0.01)
-        Bdegrade = self.smoldyn.addReaction("Bdegrade", subs=[B], prds=[], rate=0.01)
-
-        # self.smoldyn.addCommand("molcount bistableout.txt", cmd_type="N", step=10)
         if self.parameters['animate']:
             self.smoldyn.addGraphics("opengl")
 
@@ -73,30 +86,44 @@ class SmoldynProcess(Process):
             timestep,
             states
     ):
-        results = self.run(timestep)
-        return {}
-
-    def run(
-            self,
-            time
-    ):
         self.smoldyn.run(
-            stop=time,
+            stop=timestep,
             dt=self.dt)
 
+        # get the data, clear the buffer
+        data = self.smoldyn.getOutputData('counts', True)
+        final_counts = data[-1]
         import ipdb;
         ipdb.set_trace()
 
         return {}
 
+
 # functions to configure and run the process
-def run_smoldyn_process(
+def test_smoldyn_process(
         animate=False
 ):
-
     # initialize the process by passing in parameters
     parameters = {
-        'animate': animate}
+        'animate': animate,
+        'species': {
+            'X': {'difc': 0, 'color': 'green', 'display_size': 3, 'solution': {'counts': 1, 'position': [5, 5]}},
+            'A': {'difc': 1, 'color': 'red', 'display_size': 3},
+            'B': {'difc': 1, 'color': 'blue', 'display_size': 3},
+            'A2': {'difc': 1, 'color': 'red', 'display_size': 5},
+            'B2': {'difc': 1, 'color': 'blue', 'display_size': 5},
+        },
+        'reactions': {
+            'express': {'subs': ['X'], 'prds': ['X', 'A', 'B'], 'rate': 1},
+            'Adimer': {'subs': ['A', 'A'], 'prds': ['A2'], 'rate': 1},
+            'Adimer_reverse': {'subs': ['A2'], 'prds': ['A', 'A'], 'rate': 1},
+            'Bdimer': {'subs': ['B', 'B'], 'prds': ['B2'], 'rate': 1},
+            'Bdimer_reverse': {'subs': ['B2'], 'prds': ['B', 'B'], 'rate': 1},
+            'AxB': {'subs': ['A2', 'B'], 'prds': ['A2'], 'rate': 1},
+            'Adegrade': {'subs': ['A'], 'prds': [], 'rate': 1},
+            'Bdegrade': {'subs': ['B'], 'prds': [], 'rate': 1},
+        },
+    }
     template_process = SmoldynProcess(parameters)
 
     # declare the initial state
@@ -111,7 +138,7 @@ def run_smoldyn_process(
 
     # run the simulation
     sim_settings = {
-        'total_time': 1000,
+        'total_time': 100,
         'initial_state': initial_state}
     output = simulate_process(
         template_process,
@@ -120,16 +147,11 @@ def run_smoldyn_process(
     return output
 
 
-def test_template_process():
-    output = run_smoldyn_process()
-    # TODO: Add assert statements to ensure correct performance.
-
-
 def main():
     out_dir = os.path.join(PROCESS_OUT_DIR, 'smoldyn')
     os.makedirs(out_dir, exist_ok=True)
 
-    output = run_smoldyn_process()
+    output = test_smoldyn_process(animate=False)
 
     # plot the simulation output
     plot_settings = {}
