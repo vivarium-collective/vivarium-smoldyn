cbff5dcdb96eeb11b71d377d06e760c4dcf19eae
Eran Agmon <agmon.eran@gmail.com>
2021-06-10 10:28:54 -0700

    clean up

--- vivarium_smoldyn/processes/smoldyn_process.py
+++ vivarium_smoldyn/processes/smoldyn_process.py
@@ -1,4 +1,6 @@
 """
+Smoldyn
+
 Execute by running: ``python vivarium_smoldyn/processes/smoldyn_process.py``
 """
 
@@ -14,7 +16,13 @@ from vivarium.core.composition import (
 from vivarium.plots.simulation_output import plot_simulation_output
 
 
-class SmoldynProcess(Process):
+class Smoldyn(Process):
+    """ Smoldyn Process
+
+    A spatial stochastic simulator for chemical reaction networks
+
+    http://www.smoldyn.org/index.html
+    """
 
     defaults = {
         'animate': False,
@@ -33,51 +41,44 @@ class SmoldynProcess(Process):
         self.dt = self.parameters['dt']
 
         # initialize the simulation
-        self.smoldyn = sm.Simulation(
+        self.simulation = sm.Simulation(
             low=self.parameters['low'],
             high=self.parameters['high'],
             types=self.parameters['boundary'],
         )
-        self.smoldyn.addOutputData('counts')
-        self.smoldyn.addCommand(cmd='molcount counts', cmd_type='E')
+        self.simulation.addOutputData('counts')
+        self.simulation.addCommand(cmd='molcount counts', cmd_type='E')
 
         # make the species
         self.species = {}
         for name, config in self.parameters['species'].items():
             print('species ' + name)
-            self.species[name] = self.smoldyn.addSpecies(name, **config)
-            # self.set_species(name, dict(config, {
-            #     'high': self.parameters['high'],
-            #     'low': self.parameters['low']}))
+            self.species[name] = self.simulation.addSpecies(name, **config)
 
+        # make the reactions
         self.reactions = {}
         for rxn_name, config in self.parameters['reactions'].items():
             substrate_names = config.pop('subs')
             product_names = config.pop('prds')
             substrates = [self.species[name] for name in substrate_names]
             products = [self.species[name] for name in product_names]
-            self.reactions[rxn_name] = self.smoldyn.addReaction(
+            self.reactions[rxn_name] = self.simulation.addReaction(
                 rxn_name,
                 subs=substrates,
                 prds=products,
-                **config
-            )
+                **config)
 
         # TODO -- configure surfaces
 
         if self.parameters['animate']:
-            self.smoldyn.addGraphics("opengl")
+            self.simulation.addGraphics('opengl')
 
-    def set_species(self, name, config):
-        self.smoldyn.runCommand(f"killmol {name}")
+    def set_uniform(self, name, config):
+        self.simulation.runCommand(f'killmol {name}')
         self.species[name].addToSolution(
             config.get('counts'),
             highpos=config.get('high'),
             lowpos=config.get('low'))
-
-    def set_state(self, state):
-        for name, config in state.items():
-            self.set_species(name, config)
 
     def ports_schema(self):
         return {
@@ -89,7 +90,7 @@ class SmoldynProcess(Process):
                     '_emit': True,
                 } for mol_id in self.parameters['species'].keys()
             },
-            # TODO -- effective rates (as a result of crowding) could be optional
+            # TODO -- effective rates (as a result of crowding). could be optional
             'effective_rates': {}
         }
 
@@ -99,36 +100,34 @@ class SmoldynProcess(Process):
             states
     ):
 
-        # TODO: take the state, and use it to configure smoldyn
-
+        # reset the molecules, at a uniform distribution
         for name, counts in states['molecules'].items():
-            self.set_species(
+            self.set_uniform(
                 name, {
                     'counts': counts,
                     'high': self.parameters['high'],
-                    'low': self.parameters['low']
-                }
-            )
+                    'low': self.parameters['low']})
 
-        self.smoldyn.run(
+        # run simulation
+        self.simulation.run(
             stop=timestep,
             dt=self.dt)
 
         # get the data, clear the buffer
-        data = self.smoldyn.getOutputData('counts', True)
+        data = self.simulation.getOutputData('counts', True)
+
+        # get the final counts for the update
         final_counts = data[-1]
-
-        # TODO -- post processing to get effective rates
-
         molecules = {}
         for index, name in enumerate(self.parameters['species'].keys(), 1):
             molecules[name] = final_counts[index] - states['molecules'][name]
 
-        import ipdb; ipdb.set_trace()
+        # TODO -- post processing to get effective rates
+
 
         return {
             'molecules': molecules,
-            'effective_rates': {},
+            # 'effective_rates': {},
         }
 
 
@@ -140,42 +139,58 @@ def test_smoldyn_process(
     parameters = {
         'animate': animate,
         'species': {
-            'X': {'difc': 0, 'color': 'green', 'display_size': 3},
-            'A': {'difc': 1, 'color': 'red', 'display_size': 3},
-            'B': {'difc': 1, 'color': 'blue', 'display_size': 3},
-            'A2': {'difc': 1, 'color': 'red', 'display_size': 5},
-            'B2': {'difc': 1, 'color': 'blue', 'display_size': 5},
-        },
+            'X': {'difc': 0},
+            'A': {'difc': 1},
+            'B': {'difc': 1},
+            'A2': {'difc': 1},
+            'B2': {'difc': 1}},
         'reactions': {
-            'express': {'subs': ['X'], 'prds': ['X', 'A', 'B'], 'rate': 1},
-            'Adimer': {'subs': ['A', 'A'], 'prds': ['A2'], 'rate': 1},
-            'Adimer_reverse': {'subs': ['A2'], 'prds': ['A', 'A'], 'rate': 1},
-            'Bdimer': {'subs': ['B', 'B'], 'prds': ['B2'], 'rate': 1},
-            'Bdimer_reverse': {'subs': ['B2'], 'prds': ['B', 'B'], 'rate': 1},
-            'AxB': {'subs': ['A2', 'B'], 'prds': ['A2'], 'rate': 1},
-            'Adegrade': {'subs': ['A'], 'prds': [], 'rate': 1},
-            'Bdegrade': {'subs': ['B'], 'prds': [], 'rate': 1},
-        },
-    }
+            'express': {
+                'subs': ['X'],
+                'prds': ['X', 'A', 'B'],
+                'rate': 1},
+            'Adimer': {
+                'subs': ['A', 'A'],
+                'prds': ['A2'],
+                'rate': 1},
+            'Adimer_reverse': {
+                'subs': ['A2'],
+                'prds': ['A', 'A'],
+                'rate': 1},
+            'Bdimer': {
+                'subs': ['B', 'B'],
+                'prds': ['B2'],
+                'rate': 1},
+            'Bdimer_reverse': {
+                'subs': ['B2'],
+                'prds': ['B', 'B'],
+                'rate': 1},
+            'AxB': {
+                'subs': ['A2', 'B'],
+                'prds': ['A2'],
+                'rate': 1},
+            'Adegrade': {
+                'subs': ['A'],
+                'prds': [],
+                'rate': 1},
+            'Bdegrade': {
+                'subs': ['B'],
+                'prds': [],
+                'rate': 1}}}
 
-    import ipdb; ipdb.set_trace()
-
-    template_process = SmoldynProcess(parameters)
-
+    process = Smoldyn(parameters)
 
     # declare the initial state
     initial_state = {
         'molecules': {
-            'X': 10
-        }
-    }
+            'X': 10}}
 
     # run the simulation
     sim_settings = {
         'total_time': 100,
         'initial_state': initial_state}
     output = simulate_process(
-        template_process,
+        process,
         sim_settings)
 
     return output
